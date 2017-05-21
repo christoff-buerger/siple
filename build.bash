34817db395970c4f34ef27ec268dd2f05fe0b20f
cbuerger <christoff.buerger@gmx.net>
2017-05-21 20:07:46 +0200

    Minor cleanup of build script.

--- build.bash
+++ build.bash
@@ -5,31 +5,21 @@
 
 # author: Christoff Bürger
 
-clear_all(){
-	rm -rf src-gen
-	rm -rf java-bin
-	mkdir src-gen
-	mkdir java-bin
-}
-
-make_syntax(){
-	java -jar tools/jflex.jar -d src-gen/siple specifications/Lexer.jflex
-	java -jar tools/beaver.jar -c -d src-gen specifications/Parser.beaver
-}
-
-make_binary(){
-	javac -deprecation -encoding utf-8 -classpath tools/beaver-rt.jar \
-		-d java-bin -sourcepath src-gen src/**/*.java src-gen/**/*.java
-	binaries=`find java-bin/*`
-	jar cfm $1 manifest.txt -C java-bin ${binaries#*/}
-}
-
-# Delete jars:
+# Clear all:
 rm *.jar
+rm -rf src-gen
+rm -rf java-bin
+mkdir src-gen
+mkdir java-bin
 
-# Build:
-clear_all
-make_syntax
+# Generate code:
+java -jar tools/jflex.jar -d src-gen/siple specifications/Lexer.jflex
+java -jar tools/beaver.jar -c -d src-gen specifications/Parser.beaver
 java -jar tools/jastadd2.jar --rewrite --package="siple.ast" --o=src-gen \
 	specifications/*.ast specifications/*.jrag
-make_binary siple.jar
+
+# Build distribution:
+javac -deprecation -encoding utf-8 -classpath tools/beaver-rt.jar \
+	-d java-bin -sourcepath src-gen src/**/*.java src-gen/**/*.java
+binaries=`find java-bin/*`
+jar cfm siple.jar manifest.txt -C java-bin ${binaries#*/}
